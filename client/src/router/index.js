9348c951c63bad5695d3a0d9dbd5275c1ecf371f
AlexeySushkov <alexey.p.sushkov@gmail.com>
2018-12-18 18:34:16 +0300

    changes for build

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Start from '@/components/pages/StartPage'
 import Posts from '@/components/pages/PostsPage'
 import NewPost from '@/components/pages/NewPost'
 import EditPost from '@/components/pages/EditPost'
